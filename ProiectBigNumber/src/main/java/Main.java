8d11d04b4d8aedab8790ee46719786442ac77c04
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2022-05-03 12:32:24 +0300

    Evaluation steps

--- ProiectBigNumber/src/main/java/Main.java
+++ ProiectBigNumber/src/main/java/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        String exp = "9 ^ 2";
+        String exp = "9 ^ 2 + ( 2 + 5 ) + 5 * 3";
         System.out.println("Infix Expression: " + exp);
         System.out.println("Postfix Expression: " + InfixToPostFix.infixToPostFix(exp));
 
         String postFixExp = InfixToPostFix.infixToPostFix(exp);
         System.out.println(PostfixEvaluation.evaluatePostfix(postFixExp));
+        System.out.println(PostfixEvaluation.evaluationSteps);
 
         ParseXMLDocument doc = new ParseXMLDocument("class.xml");
         doc.parseFileXML();
--- ProiectBigNumber/src/main/java/PostfixEvaluation.java
+++ ProiectBigNumber/src/main/java/PostfixEvaluation.java
@@ -3,13 +3,20 @@ import java.util.*;
 public class PostfixEvaluation
 {
 
+    public static String evaluationSteps = "";
+
     static boolean isOperator(String operator)
     {
         return operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/") || operator.equals("^");
     }
 
+    static String buildExpression(String operator, String op1, String op2, String result){
+       return op1 + " " + operator + " " + op2 + " = " + result +  "\n";
+    }
+
     static BigNumber evaluatePostfix(String expression)
     {
+        StringBuilder steps = new StringBuilder();
         String[] exp = expression.split(" ");
         Stack<BigNumber> postFix = new Stack<>();
         int n = exp.length;
@@ -20,22 +27,38 @@ public class PostfixEvaluation
             {
                 BigNumber op1 = postFix.pop();
                 BigNumber op2 = postFix.pop();
+                BigNumber result;
 
                 switch(exp[i])
                 {
-                    case "+": postFix.push(new BigNumber(BigNumber.add(op1.getValue(), op2.getValue())));
+                    case "+":
+                        result = new BigNumber(BigNumber.add(op1.getValue(), op2.getValue()));
+                        postFix.push(result);
+                        steps.append(buildExpression("+", op1.getValue(), op2.getValue(), result.getValue()));
                         break;
 
-                    case "-": postFix.push(new BigNumber(BigNumber.substract(op2.getValue(), op1.getValue())));
+                    case "-":
+                        result = new BigNumber(BigNumber.substract(op2.getValue(), op1.getValue()));
+                        postFix.push(result);
+                        steps.append(buildExpression("-", op2.getValue(), op1.getValue(), result.getValue()));
                         break;
 
-                    case "*": postFix.push(new BigNumber(BigNumber.multiply(op1.getValue(), op2.getValue())));
+                    case "*":
+                        result = new BigNumber(BigNumber.multiply(op1.getValue(), op2.getValue()));
+                        postFix.push(result);
+                        steps.append(buildExpression("*", op1.getValue(), op2.getValue(), result.getValue()));
                         break;
 
-                    case "/": postFix.push(new BigNumber(BigNumber.divide(op2.getValue(), Integer.parseInt(op1.getValue()))));
+                    case "/":
+                        result = new BigNumber(BigNumber.divide(op2.getValue(), Integer.parseInt(op1.getValue())));
+                        postFix.push(result);
+                        steps.append(buildExpression("/", op2.getValue(), op1.getValue(), result.getValue()));
                         break;
 
-                    case "^": postFix.push(new BigNumber(BigNumber.power(op2.getValue(), Long.parseLong(op1.getValue()))));
+                    case "^":
+                        result = new BigNumber(BigNumber.power(op2.getValue(), Long.parseLong(op1.getValue())));
+                        postFix.push(result);
+                        steps.append(buildExpression("^", op2.getValue(), op1.getValue(), result.getValue()));
                         break;
                 }
 
@@ -43,12 +66,12 @@ public class PostfixEvaluation
 
             else
             {
-//                int operand = Integer.parseInt(exp[i]);
+
                 BigNumber operand = new BigNumber(exp[i]);
                 postFix.push(operand);
             }
         }
-
+        evaluationSteps = steps.toString();
         return postFix.pop();
     }
 }
